53ee79cdc43117180f3343eeaa88889cff554fbc
forest93 <forest93@163.com>
2019-09-16 22:42:22 +0800

    Layout.

--- src/pages/familyTree.tsx
+++ src/pages/familyTree.tsx
@@ -1,4 +1,4 @@
-import { Slider, Typography } from "@material-ui/core";
+import { Grid, Paper, Slider, Typography } from "@material-ui/core";
 import * as React from "react";
 import { match } from "react-router";
 import { FamilyTree as FamilyTreeComponent, IFamilyTreeNode } from "../components/FamilyTree";
@@ -18,17 +18,13 @@ function renderNode(this: IFamilyTreeNode): React.ReactNode {
     return <RdfEntityLabel qName={this.id} />;
 }
 
-function walk(characterId: RdfQName, maxDistanceUp?: number, maxDistanceDown?: number): IFamilyTreeNode[] {
-    if (maxDistanceUp && maxDistanceUp < 0)
+function walk(characterId: RdfQName, maxDistance?: number): IFamilyTreeNode[] {
+    if (maxDistance && maxDistance < 0)
         throw new RangeError("maxDistanceUp should be non-negative number.");
-    if (maxDistanceDown && maxDistanceDown < 0)
-        throw new RangeError("maxDistanceDown should be non-negative number.");
     const q: [number, RdfQName, RdfQName?][] = [[0, characterId]];
     const edgeTypes = new Set<CharacterRelationType>(["parent", "child"]);
     const nodeMap = new Map<RdfQName, IFamilyTreeNode>();
     while (q.length > 0) {
-        // distance > 0: Down
-        // distance < 0: Up
         const [distance, charId, reachedFrom] = q.shift()!;
         if (nodeMap.has(charId)) continue;
         const node: IFamilyTreeNode & { distance?: number, reachedFrom?: string } = {
@@ -41,14 +37,13 @@ function walk(characterId: RdfQName, maxDistanceUp?: number, maxDistanceDown?: n
         const relations = dataService.getRelationsFor(charId, edgeTypes);
         if (!relations) continue;
         for (const relation of relations) {
-            if (relation.relation === "parent" && (maxDistanceUp == null || distance - 1 >= -maxDistanceUp)) {
+            if (maxDistance != null && distance + 1 > maxDistance) continue;
+            if (relation.relation === "parent") {
                 if (node.parentId1 == null) node.parentId1 = relation.target;
                 else if (node.parentId2 == null) node.parentId2 = relation.target;
                 else console.warn(`${charId} has more than 2 parents.`);
-                if (!nodeMap.has(relation.target)) {
-                    q.push([distance - 1, relation.target, charId]);
-                }
-            } else if (relation.relation === "child" && (maxDistanceDown == null || distance + 1 <= maxDistanceDown) && !nodeMap.has(relation.target)) {
+            }
+            if (!nodeMap.has(relation.target)) {
                 q.push([distance + 1, relation.target, charId]);
             }
         }
@@ -58,13 +53,13 @@ function walk(characterId: RdfQName, maxDistanceUp?: number, maxDistanceDown?: n
 
 export const FamilyTree: React.FC<IFamilyTreeProps> = (props) => {
     let characterId = props.match.params.character;
-    const [depth, setDepth] = React.useState(3);
+    const [maxDistance, setMaxDistance] = React.useState(3);
     const [familyTreeNodes, setFamilyTreeNodes] = React.useState<IFamilyTreeNode[]>([]);
     React.useEffect(() => {
         if (!characterId) return;
-        const nodes = walk(characterId, depth, depth);
+        const nodes = walk(characterId, maxDistance);
         setFamilyTreeNodes(nodes);
-    }, [characterId, depth]);
+    }, [characterId, maxDistance]);
     if (!characterId) {
         return <span>No character ID specified.</span>;
     }
@@ -72,8 +67,14 @@ export const FamilyTree: React.FC<IFamilyTreeProps> = (props) => {
     return (<React.Fragment>
         <h1>Family tree of <RdfEntityLabel qName={characterId} showEntityId={true} /></h1>
         <Typography variant="subtitle1"><RdfEntityDescription qName={characterId} /></Typography>
-        <Typography id="max-depth-slider" gutterBottom>Max depth: {depth}</Typography>
-        <Slider aria-labelledby="discrete-slider" valueLabelDisplay="auto" marks value={depth} step={1} min={1} max={6} onChange={(e, v) => setDepth(v as number)} />
-        <FamilyTreeComponent nodes={familyTreeNodes} />
+        <Grid container spacing={1}>
+            <Grid item xs={12} md={6} lg={4}>
+                <Typography id="max-depth-slider">Max depth: {maxDistance}</Typography>
+                <Slider aria-labelledby="discrete-slider" marks value={maxDistance} step={1} min={1} max={30} onChange={(e, v) => setMaxDistance(v as number)} />
+            </Grid>
+        </Grid>
+        <Paper>
+            <FamilyTreeComponent nodes={familyTreeNodes} />
+        </Paper>
     </React.Fragment>);
 };
