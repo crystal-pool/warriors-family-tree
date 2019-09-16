6d1f8e87af6ac701e0787808b86260d896d16b76
forest93 <forest93@163.com>
2019-09-16 21:59:35 +0800

    Bug fix.

--- src/pages/familyTree.tsx
+++ src/pages/familyTree.tsx
@@ -23,26 +23,33 @@ function walk(characterId: RdfQName, maxDistanceUp?: number, maxDistanceDown?: n
         throw new RangeError("maxDistanceUp should be non-negative number.");
     if (maxDistanceDown && maxDistanceDown < 0)
         throw new RangeError("maxDistanceDown should be non-negative number.");
-    const q: [number, RdfQName][] = [[0, characterId]];
+    const q: [number, RdfQName, RdfQName?][] = [[0, characterId]];
     const edgeTypes = new Set<CharacterRelationType>(["parent", "child"]);
     const nodeMap = new Map<RdfQName, IFamilyTreeNode>();
     while (q.length > 0) {
         // distance > 0: Down
         // distance < 0: Up
-        const [distance, charId] = q.shift()!;
+        const [distance, charId, reachedFrom] = q.shift()!;
         if (nodeMap.has(charId)) continue;
-        const node: IFamilyTreeNode = { id: charId, render: renderNode };
+        const node: IFamilyTreeNode & { distance?: number, reachedFrom?: string } = {
+            id: charId,
+            render: renderNode,
+            // distance,
+            // reachedFrom
+        };
         nodeMap.set(charId, node);
         const relations = dataService.getRelationsFor(charId, edgeTypes);
         if (!relations) continue;
         for (const relation of relations) {
-            if (relation.relation === "parent" && (maxDistanceUp == null || distance - 1 >= -maxDistanceUp) && !nodeMap.has(relation.target)) {
+            if (relation.relation === "parent" && (maxDistanceUp == null || distance - 1 >= -maxDistanceUp)) {
                 if (node.parentId1 == null) node.parentId1 = relation.target;
                 else if (node.parentId2 == null) node.parentId2 = relation.target;
                 else console.warn(`${charId} has more than 2 parents.`);
-                q.push([distance - 1, relation.target]);
+                if (!nodeMap.has(relation.target)) {
+                    q.push([distance - 1, relation.target, charId]);
+                }
             } else if (relation.relation === "child" && (maxDistanceDown == null || distance + 1 <= maxDistanceDown) && !nodeMap.has(relation.target)) {
-                q.push([distance + 1, relation.target]);
+                q.push([distance + 1, relation.target, charId]);
             }
         }
     }
