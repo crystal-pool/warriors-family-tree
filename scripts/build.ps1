47ee0a6eb1ce3356522bd74873fe8836aa6b415c
forest93 <forest93@163.com>
2019-10-23 23:29:49 +0800

    Try GitHub Actions!

---
+++ scripts/build.ps1
@@ -0,0 +1,31 @@
+#!/usr/bin/env pwsh
+
+param (
+    [Parameter()]
+    [string]
+    $RdfPath = "../Dump/wbdump.ttl"
+)
+
+function checkLastExitCode() {
+    if ($LASTEXITCODE) {
+        Write-Error "Command exit code indicates failure: $LASTEXITCODE"
+        Exit $LASTEXITCODE
+    }
+}
+
+$ErrorActionPreference = "Stop"
+
+$RdfPath = (Resolve-Path $RdfPath).Path
+$DataPath = (New-Item "./assets/data" -ItemType Directory -Force).FullName
+$DataBuilderProjectDir = (Resolve-Path "./DataBuilder/DataBuilder.csproj").Path
+
+# Assumes $PWD is repo root
+dotnet build -c Release $DataBuilderProjectDir
+checkLastExitCode
+dotnet run -c Release -p $DataBuilderProjectDir -- $RdfPath $DataPath
+checkLastExitCode
+
+yarn install
+checkLastExitCode
+yarn build-prod
+checkLastExitCode
---
+++ scripts/prep.ps1
@@ -0,0 +1,35 @@
+#!/usr/bin/env pwsh
+
+function checkLastExitCode() {
+    if ($LASTEXITCODE) {
+        Write-Error "Command exit code indicates failure: $LASTEXITCODE"
+        Exit $LASTEXITCODE
+    }
+}
+
+$ErrorActionPreference = "Stop"
+
+[string[]]$sdks = dotnet --list-sdks
+checkLastExitCode
+
+$matchingSdk = $sdks | ? { $_ -match "^[3-9]\." }
+
+Write-Host "Installed supported .NET Core SDK:"
+Write-Host $matchingSdk
+
+if (-not $matchingSdk) {
+    Write-Host "Installing .NET Core SDK."
+    Invoke-WebRequest https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -OutFile packages-microsoft-prod.deb
+    sudo dpkg -i packages-microsoft-prod.deb
+    checkLastExitCode
+    sudo add-apt-repository universe
+    checkLastExitCode
+    sudo apt-get update
+    checkLastExitCode
+    sudo apt-get install apt-transport-https
+    checkLastExitCode
+    sudo apt-get update
+    checkLastExitCode
+    sudo apt-get install dotnet-sdk-3.0
+    checkLastExitCode
+}
