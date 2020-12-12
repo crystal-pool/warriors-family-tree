546536bef5013a9af9d55dfb7216c334a16b02a3
forest93 <forest93@163.com>
2020-12-12 18:40:41 +0800

    Update workflow script for install dotnet SDK.

--- scripts/prepEnv.ps1
+++ scripts/prepEnv.ps1
@@ -14,24 +14,7 @@ $ErrorActionPreference = "Stop"
 [string[]]$sdks = dotnet --list-sdks
 checkLastExitCode
 
-$matchingSdk = $sdks | ? { $_ -match "^[3-9]\." }
+$matchingSdk = $sdks | ? { $_ -match "^[5-9]\." }
 
 Write-Host "Installed supported .NET Core SDK:"
 Write-Host $matchingSdk
-
-if (-not $matchingSdk) {
-    Write-Host "Installing .NET Core SDK."
-    Invoke-WebRequest https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -OutFile packages-microsoft-prod.deb
-    sudo dpkg -i packages-microsoft-prod.deb
-    checkLastExitCode
-    sudo add-apt-repository universe
-    checkLastExitCode
-    sudo apt-get update
-    checkLastExitCode
-    sudo apt-get install apt-transport-https
-    checkLastExitCode
-    sudo apt-get update
-    checkLastExitCode
-    sudo apt-get install dotnet-sdk-3.0
-    checkLastExitCode
-}
