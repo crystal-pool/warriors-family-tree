12168a7810c09c8bb00d5955015106a12abedd2e
CrystalPoolBot <crystalpool-lab@outlook.com>
2026-05-10 15:21:19 +0000

    Deploy app for crystal-pool/warriors-family-tree@9e17d65be545e0ad0b452d8534bf7911ba495dfd .

--- embed/embed/src/pages.d.ts
+++
@@ -1,6 +0,0 @@
-import { IDisposable } from "tasklike-promise-library";
-export interface IFamilyTreeOptions {
-    qName: string;
-    depth: number;
-}
-export declare function mountFamilyTree(container: HTMLElement, options: IFamilyTreeOptions): IDisposable;
